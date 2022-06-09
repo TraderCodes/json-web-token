e7685048bb58665e720d2cd7968cb98c0aa646f6
TraderCodes <85665271+TraderCodes@users.noreply.github.com>
2022-06-09 12:43:51 -0700

    .

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,9 @@
-const { CustomAPIError } = require('../errors')
-const { StatusCodes } = require('http-status-codes')
+const CustomAPIError = require('../errors/custom-error')
 const errorHandlerMiddleware = (err, req, res, next) => {
   if (err instanceof CustomAPIError) {
     return res.status(err.statusCode).json({ msg: err.message })
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+  return res.status(500).send('Something went wrong try again later')
 }
 
 module.exports = errorHandlerMiddleware
