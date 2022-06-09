8aca4d350300e5f4c59c4c1ef17ff7545e436855
TraderCodes <85665271+TraderCodes@users.noreply.github.com>
2022-06-09 14:08:12 -0700

    Setup JsonWebtoken

--- controllers/main.js
+++ controllers/main.js
@@ -1,20 +1,20 @@
-
-
-const CustomAPIError = require ('../errors/custom-error')
+const jwt = require('jsonwebtoken');
+const CustomAPIError = require('../errors/custom-error');
 const login = async (req, res) => {
-
-
   // find the value that user enter
   const { username, password } = req.body;
   // check if password and username are there
-if(!username || !password){
-  throw new CustomAPIError('please enter a username and password',400)
+  if (!username || !password) {
+    throw new CustomAPIError('please enter a username and password', 400);
+  }
+  const id = new Date().getDate();
+  // return token
+  // pass in secret in the second param
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
 
-
-
-}
-
-  res.send('fake login ');
+  res.status(200).json({ msg: 'user created account', token });
 };
 const dashboard = async (req, res) => {
   const luckyNumber = Math.floor(Math.random() * 100);
